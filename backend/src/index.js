36e5e107654210ad66aa4b615867b863688ace54
kishan dev sah <sonofsahuji@gmail.com>
2026-09-11 18:42:52 +0530

    updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ app.use(cors(
     {
         origin: ['http://localhost:5173',
             'https://iit-jee-instuite.vercel.app',
-            'http://kdclasses.vercel.app/'],
+            'https://kdclasses.vercel.app'],
         credentials: true
     }
 ))
